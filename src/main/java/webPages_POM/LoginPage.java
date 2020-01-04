13e02e2e492e563d1521f09646e13d80740a0e0a
AthiraAnanth <athiras.a@gmail.com>
2020-01-03 18:59:24 -0800

    Login Tests Initial scripts

--- src/main/java/webPages_POM/LoginPage.java
+++ src/main/java/webPages_POM/LoginPage.java
@@ -1,5 +1,41 @@
 package webPages_POM;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
 
+	WebDriver driver;
+	@FindBy(xpath="//input[@name='uid']")
+	private WebElement userId;
+	@FindBy(xpath="//input[@name='password']")
+	private WebElement password;
+	@FindBy(css="input[name='btnLogin'")
+	private WebElement login;
+	@FindBy(css="input[name='btnReset'")
+	private WebElement reset;
+	
+	
+	public LoginPage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void setUser_Id(String username) {
+		userId.sendKeys(username);
+	}
+	
+	public void setPassword(String pass) {
+		password.sendKeys(pass);
+	}
+	
+	public void click_Login() {
+		login.click();
+	}
+	
+	public void click_Reset() {
+		reset.click();
+	}
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,5 +1,34 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import webPages_POM.LoginPage;
+
 public class LoginTests {
+	
+private static WebDriver driver;
+	
+	@BeforeTest
+	public static void initDriver() {
+		System.setProperty("webdriver.chrome.driver", "F:\\Athira\\Selenium\\Selenium 3.141.59\\Driver\\chromedriver.exe");
+		driver= new ChromeDriver();
+		driver.get(" http://www.demo.guru99.com/V4/");
+		driver.manage().window().maximize();
+	}
+	
+	
+	@Test
+	public static void LoginTest() {
+		LoginPage lPage= new LoginPage(driver);
+		lPage.setUser_Id("mngr240444");
+		lPage.setPassword("zyjAgUn");
+		lPage.click_Login();
+		String expectedHPage="http://www.demo.guru99.com/V4/manager/Managerhomepage.php";
+		Assert.assertEquals(driver.getCurrentUrl(), expectedHPage);
+	}
 
 }
